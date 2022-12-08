74c7ada896accaf0a02d96f281beb3725484fd8d
JESSICA JINNETH ESCAMILLA CARRERO <jescamilla02@unisalle.edu.co>
2022-12-07 20:14:46 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
-    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // you can replace this port with any port
   },
   plugins: [react()]
 })
